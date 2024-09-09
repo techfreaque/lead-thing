d6d3921c62443cb857850d739a43bea407060a88
Max <max@tfq.at>
2024-09-09 11:23:57 +0200

    Send DOI mail for cleverreach

--- app/api/cleverreach/route.ts
+++ app/api/cleverreach/route.ts
@@ -5,7 +5,8 @@ import { ApiResponse, formatApiCallDetails } from '@/app/_lib/apiHelpers';
 import executeIfAuthenticated from '../../_server/apiHelpers';
 import type { CleverreachPostRequest } from '../requestTypes';
 
-const apiContactsUrl = 'https://rest.cleverreach.com/groups/{group_id}/receivers';
+const apiContactsUrl = 'https://rest.cleverreach.com/v3/groups.json/{group_id}/receivers';
+const apiDOIMailUrl = 'https://rest.cleverreach.com/v3/forms.json/{form_id}/send/activate';
 const oAuthUrl = 'https://rest.cleverreach.com/oauth/token.php';
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
@@ -19,6 +20,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     // salutation,
     subscriptionMode,
     cleverreachListId,
+    cleverreachFormId,
     cleverreachSource,
     cleverreachClientId,
     cleverreachClientSecret,
@@ -28,7 +30,9 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
       const authResponse = await fetch(oAuthUrl, {
         method: 'POST',
         headers: {
-          Authorization: `Basic ${Buffer.from(`${cleverreachClientId}:${cleverreachClientSecret}`).toString('base64')}`,
+          Authorization: `Basic ${Buffer.from(
+            `${cleverreachClientId}:${cleverreachClientSecret}`
+          ).toString('base64')}`,
           'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: new URLSearchParams({
@@ -40,16 +44,16 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
         return await createLead(authData.access_token);
       }
       return ApiResponse(
-        `Failed to get authentication token. Error: ${JSON.stringify(authData)} ${formatApiCallDetails(
-          {
-            firstname,
-            lastname,
-            email,
-            subscriptionMode,
-            cleverreachListId,
-            cleverreachSource,
-          }
-        )}`,
+        `Failed to get authentication token. Error: ${JSON.stringify(
+          authData
+        )} ${formatApiCallDetails({
+          firstname,
+          lastname,
+          email,
+          subscriptionMode,
+          cleverreachListId,
+          cleverreachSource,
+        })}`,
         500
       );
     } catch (error) {
@@ -91,10 +95,15 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
       );
       const data = await response.json();
       if (response.ok) {
-        return ApiResponse(
-          `Contact successfully added and subscribed! Response: ${JSON.stringify(data)}`,
-          200
-        );
+        if (!cleverreachFormId) {
+          return ApiResponse(
+            `Contact successfully added and subscribed, BUT NO DOI MAIL WAS SENT! Response: ${JSON.stringify(
+              data
+            )}`,
+            200
+          );
+        }
+        return await subscribeToList(authToken);
       }
       return ApiResponse(
         `Failed to add the contact. Error: ${JSON.stringify(data)} ${formatApiCallDetails({
@@ -117,6 +126,64 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
           cleverreachListId,
           cleverreachSource,
         })}`,
+        500
+      );
+    }
+  }
+  async function subscribeToList(authToken: string): Promise<NextResponse> {
+    try {
+      const response: Response = await fetch(
+        apiDOIMailUrl.replace('{form_id}', cleverreachFormId),
+        {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${authToken}`,
+          },
+          body: JSON.stringify({
+            email,
+            doidata: {
+              user_ip: '127.0.0.1',
+              referer: 'leadthing.dev',
+              user_agent: 'Linux',
+            },
+          }),
+        }
+      );
+      const data = await response.json();
+      if (response.ok) {
+        return ApiResponse(
+          `Contact successfully added, subscribed and DOI mail sent! Response: ${JSON.stringify(
+            data
+          )}`,
+          200
+        );
+      }
+      return ApiResponse(
+        `Contact added, but failed to send DOI mail. Error: ${JSON.stringify(
+          data
+        )} ${formatApiCallDetails({
+          firstname,
+          lastname,
+          email,
+          subscriptionMode,
+          cleverreachListId,
+          cleverreachSource,
+        })}`,
+        500
+      );
+    } catch (error) {
+      return ApiResponse(
+        `Contact added, but failed to send DOI mail with an unknown error. Error: ${error} ${formatApiCallDetails(
+          {
+            firstname,
+            lastname,
+            email,
+            subscriptionMode,
+            cleverreachListId,
+            cleverreachSource,
+          }
+        )}`,
         500
       );
     }
