ea6b684a00e96fef8f19d572ee5bff348cd07381
Max B <max@tfq.at>
2025-09-15 17:58:51 +0200

    remove front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-import GetInTouch from '@/app/_components/GetInTouch/GetInTouch';
-import HomeFeatures from '@/app/_components/HomeFeatures/HomeFeatures';
-import HomeHero from '@/app/_components/HomeHero/HomeHero';
-
-import SubscriptionTiersSection from './_components/SubscriptionTiers/SubscriptionTiers';
 
 export default function HomePage() {
   return (
     <>
-      <HomeHero />
+      {/* <HomeHero />
       <HomeFeatures />
       <SubscriptionTiersSection />
-      {/* <Faq /> */}
-      <GetInTouch />
+      <GetInTouch /> */}
     </>
   );
 }
